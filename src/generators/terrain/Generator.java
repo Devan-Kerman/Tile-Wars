48791661e44e89187f340d292a70f0bc71f0a28a
Devan-Kerman <devan@cleverpath.com>
2018-11-23 11:43:52 -0600

    orgin

--- src/generators/terrain/Generator.java
+++ src/generators/terrain/Generator.java
@@ -2,10 +2,10 @@ package generators.terrain;
 
 import tile.Tile;
 
-public class Generator { // Class for Map generation
+public class Generator {
 	public static Terrain tg = new Terrain(7, 0.6f, 0.0050f); //Terrain generator
 	public static Terrain hg = new Terrain(7, 0.6f, 0.0050f); //Humidity generator
-	//public static final int chunksize = 1000; //Size of chunk
+	
 	public Tile generate(int x, int y) {
 		Tile t = new Tile(x,y);
 		return t;
--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,6 +1,23 @@
 package tile;
 
 public class Tile {
+	public int x; // X
+	public int y; // Y
+	public byte elevation; // 'Z' (Elevation)
+	public byte iron_ore;
+	public byte bauxite_ore;
+	public byte tin_ore;
+	public byte copper_ore;
+	public byte gold_ore;
+	public byte silver_ore;
+	public byte coal_ore;
+	public byte platinum_ore;
+	public byte nat_gas;
+	public byte coal;
+	public byte oil;
+	public byte gems;
+	public byte wildlife;
+	public byte lumber;
 	public Tile(int x, int y) {
 		
 	}
