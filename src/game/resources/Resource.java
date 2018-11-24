150d2a79a5ef8c4687d3efe165363d4b574b1e06
Devan-Kerman <devan@cleverpath.com>
2018-11-24 14:22:04 -0600

    commit

--- src/game/resources/Resource.java
+++ src/game/resources/Resource.java
@@ -1,12 +1,30 @@
 package game.resources;
 
 public class Resource {
+<<<<<<< HEAD
 	public String resourceID;
 	public int amount;
 
 	public Resource(String resourceID, final int amount) {
+=======
+	private final String resourceID;
+	private int amount;
+	
+	public Resource(final String resourceID, final int amount) {
+>>>>>>> parent of 8d1b4b2... Merge branch 'master' of https://github.com/Devan-Kerman/Tile-Wars
 		this.resourceID = resourceID;
-		this.amount = amount;
+		this.setAmount(amount);
 	}
 
+	public String getResourceID() {
+		return resourceID;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(final int amount) {
+		this.amount = amount;
+	}
 }
