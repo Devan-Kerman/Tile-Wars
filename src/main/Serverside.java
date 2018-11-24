e7e770d2782f0163090f69e638d26a87783e9b75
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2018-11-24 10:36:37 -0600

    Added logging

--- src/main/Serverside.java
+++ src/main/Serverside.java
@@ -3,20 +3,18 @@ package main;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.text.SimpleDateFormat;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import util.datamanagement.manager.ChunkManager;
 import visualizer.VPanel;
 
 public class Serverside {
-	private static final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("hh:mm:ss");
+	private static final Logger LOGGER = LoggerFactory.getLogger(Serverside.class);
 	
 	public static void main(String[] args) throws IOException {
 		cmds();
@@ -28,27 +26,26 @@ public class Serverside {
 		while (true) {
 			t = new Scanner(s.nextLine());
 			String temp = t.next();
-			if (temp.equals("close")) {
+			if (temp.equals("close") || temp.equals("Exit")) {
 				break;
 			} else if (temp.equals("help")) {
-				System.out.println("avilable commands:");
-				System.out.println("close - closes application");
-				System.out.println("help - shows this list");
-				System.out.println("render <Xcoord> <Ycoord> - render a specific chunck at specified coordinates");
-				System.out.println();
+				LOGGER.info("avilable commands:");
+				LOGGER.info("close - closes application");
+				LOGGER.info("help - shows this list");
+				LOGGER.info("render <Xcoord> <Ycoord> - render a specific chunck at specified coordinates");
 			} else if (temp.equals("render")) {
 				try {
 					int x = t.nextInt();
 					int y = t.nextInt();
 					Visuals.visualize(x, y);
 					//insert future code here
-					System.out.println("Sucessfully rendered chunk at (" + x + ", " + y + ")");
+					LOGGER.info("Sucessfully rendered chunk at (" + x + ", " + y + ")");
 				}catch(InputMismatchException e)
 				{
-					System.out.println("Invalid command, usage : render <Xcord> <Ycord>");
+					LOGGER.info("Invalid command, usage : render <Xcord> <Ycord>");
 				}catch(NoSuchElementException e)
 				{
-					System.out.println("Invalid command, usage : render <Xcord> <Ycord>");
+					LOGGER.info("Invalid command, usage : render <Xcord> <Ycord>");
 				}
 			} else if (temp.equals("export")) {
 				try {
@@ -59,21 +56,18 @@ public class Serverside {
 					new VPanel(ChunkManager.getSafe(x, y).data).Export(f);
 					Desktop.getDesktop().open(f);
 					//insert future code here
-					System.out.println("Sucessfully exported chunk at (" + x + ", " + y + ")");
+					LOGGER.info("Sucessfully exported chunk at (" + x + ", " + y + ")");
 				}catch(InputMismatchException e)
 				{
-					System.out.println("Invalid command, usage : render <Xcord> <Ycord>");
+					LOGGER.info("Invalid command, usage : export <Xcord> <Ycord>");
 				}catch(NoSuchElementException e)
 				{
-					System.out.println("Invalid command, usage : render <Xcord> <Ycord>");
+					LOGGER.info("Invalid command, usage : export <Xcord> <Ycord>");
 				}
 
-				System.out.println();
-			} else if(temp.equals("Exit")) {
-				break;
 			}
 			else
-				System.out.println("nvalid command, try \"help\" for a list of avilable commands");
+				LOGGER.info("invalid command, try \"help\" for a list of avilable commands");
 		}
 		t.close();
 		s.close();
