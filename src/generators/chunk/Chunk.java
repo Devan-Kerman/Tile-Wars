3998149ed6f2561cb4424901aa67d1653c7c2a96
Devan-Kerman <devan@cleverpath.com>
2018-11-24 10:24:44 -0600

    Changes
    feeede

--- src/generators/chunk/Chunk.java
+++ src/generators/chunk/Chunk.java
@@ -1,15 +1,20 @@
 package generators.chunk;
 
+import java.awt.Point;
+
 import generators.terrain.Generator;
 import tile.Tile;
 
 public class Chunk {
 	public Tile[][] data;
-	public static int chunksize = 100;
+	public static int chunksize = 1000;
+	public Point coors;
+	
 	public Chunk(int cx, int cy) {
 		data = new Tile[chunksize][chunksize];
 		for(int x = 0; x < chunksize; x++)
 			for (int y = 0; y < chunksize; y++)
 				data[x][y] = Generator.generate(cx*chunksize + x, cy*chunksize + y);
+		coors = new Point(cx,cy);
 	}
 }
--- src/util/datamanagement/manager/ChunkManager.java
+++ src/util/datamanagement/manager/ChunkManager.java
@@ -1,6 +1,14 @@
 package util.datamanagement.manager;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import generators.chunk.Chunk;
 import tile.Tile;
 import util.datamanagement.maps.MaxMap;
@@ -22,16 +30,20 @@ public class ChunkManager {
 	public static Chunk getUnsafe(int x, int y) {
 		return chunks.get(x).get(y);
 	}
-	
 	public static boolean inChunks(int x, int y) {
 		return chunks.containsKey(x) && chunks.get(x).containsKey(y);
 	}
-	
 	public static Chunk getSafe(int x, int y) {
-		
 		Chunk c;
 		if(inChunks(x,y)) {
 			c = getUnsafe(x,y);
+		}
+		else if(inDrive(x,y)) {
+			try {
+				c = read(x,y);
+			} catch(Exception e) {
+				c = null;
+			}
 		}
 		else {
 			c = gen(x,y);
@@ -43,5 +55,25 @@ public class ChunkManager {
 	public static Tile getTile(int tx, int ty) {
 		Chunk c = getSafe(tx/Chunk.chunksize, ty/Chunk.chunksize);
 		return c.data[tx%Chunk.chunksize][ty%Chunk.chunksize];
+	}
+	
+	public static boolean inDrive(int x, int y) {
+		return new File("Chunkdata/["+x+","+y+"].chunk").exists();
+	}
+	
+	public static void write(Chunk c) throws FileNotFoundException, IOException {
+		File f = new File("Chunkdata/["+c.coors.x+","+c.coors.y+"].temp");
+		f.delete();
+		f.createNewFile();
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(f));
+		oos.writeObject(c);
+		oos.close();
+		f.renameTo(new File("Chunkdata/["+c.coors.x+","+c.coors.y+"].chunk"));
+	}
+	public static Chunk read(int x, int y) throws FileNotFoundException, IOException, ClassNotFoundException {
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("Chunkdata/["+x+","+y+"].chunk"));
+		Chunk c = (Chunk) ois.readObject();
+		ois.close();
+		return c;
 	}
 }
